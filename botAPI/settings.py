2177f12aa6503b98839389fa3c3d527288711324
MatiasRei <matiasreimondo@gmail.com>
2019-05-31 00:21:38 -0300

    Fix database

--- botAPI/settings.py
+++ botAPI/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,16 +74,8 @@ WSGI_APPLICATION = 'botAPI.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'bot',
-        'USER': 'mati',
-        'PASSWORD': 'redlive4582',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
-    }
-}
+
+DATABASES['default'] =  dj_database_url.config()
 
 
 # Password validation
