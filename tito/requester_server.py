6352ea63d145d9539e03569b748ba54e94aeb22e
MatiasRei <matiasreimondo@gmail.com>
2019-06-01 19:52:56 -0300

    reuqesters fix

--- tito/requester_server.py
+++ tito/requester_server.py
@@ -10,13 +10,13 @@ class RequesterServer:
     TYPE_TEXT = "2"
 
     def user_info(self,username):
-        url = BASIC_URL + USERS_URL + "/" + username
-        response = requests.get( url, headers={'x-auth-token': TOKEN})
+        url = self.BASIC_URL + self.USERS_URL + "/" + username
+        response = requests.get( url, headers={'x-auth-token': self.TOKEN})
         json_response = response.json()
 
     def send_message(self, message, username, workspace, channel):
-        url = BASIC_URL + MESSAGE_URL + "/workspace/" + workspace + "/channel/" + channel
-        response = requests.post(url,data={'creator':'value', 'text':message, 'type': TYPE_TEXT}, headers={'x-auth-token': TOKEN})
+        url = self.BASIC_URL + self.MESSAGE_URL + "/workspace/" + workspace + "/channel/" + channel
+        response = requests.post(url,data={'creator':'value', 'text':message, 'type': self.TYPE_TEXT}, headers={'x-auth-token': self.TOKEN})
 
         response.raise_for_status()
 
